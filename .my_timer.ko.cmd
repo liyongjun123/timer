1994e270a3328945d23236b57b08e1e3b5506510
wifi_group <wifi_group@ubuntu16.04>
2020-06-30 11:17:29 +0800

    adapt timer_setup

--- .my_timer.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wifi_group/liyongjun/timer/my_timer.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/wifi_group/liyongjun/timer/my_timer.ko /home/wifi_group/liyongjun/timer/my_timer.o /home/wifi_group/liyongjun/timer/my_timer.mod.o ;  true
